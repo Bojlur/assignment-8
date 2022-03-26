03c81bfde7374550dfcfeee1ab9c8a46fb4361d0
Bajlur Rahman <bajlurrahman11@gmail.com>
2022-03-26 22:35:11 +0600

    text are added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './component/Header/Header';
 import Shop from './component/Shop/Shop';
+import Text from './component/Text/Text';
 
 function App() {
   return (
     <div className="App">
         <Header></Header>
         <Shop></Shop>
+        <Text></Text>
     </div>
   );
 }
---
+++ src/component/Text/Text.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Text = () => {
+    return (
+        <div>
+            <div>
+                <h2>How React works</h2>
+                <p>React is a java Script library that creates user interfaces
+                    in a predicatble and efficient way using declarative code. 
+                    we use react component to break down code and create component 
+                    display the information.
+                </p>
+            </div>
+                <h2>Difference between props and State</h2>
+                <p>
+                    The key difference between props and state is that state is 
+                    internal and controlled by the component itself while props are 
+                    external and controlled by whetever renders the component.
+                </p>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Text;
